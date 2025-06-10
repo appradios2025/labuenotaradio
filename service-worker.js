cf8a13a15d79639263f698f89b6ac160d61312dd
appradios2025 <fmluisorlando@gmail.com>
2025-06-10 16:05:32 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,11 @@
-
-const CACHE_NAME = 'la-buenota-cache-v1';
-const urlsToCache = [
-  '.',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './fondobuenota.png',
-  './vinilodisco.png'
-];
-
-// Instalación y almacenamiento en caché
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service Worker instalado.');
 });
 
-// Activación y limpieza de cachés antiguas
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+  console.log('Service Worker activado.');
 });
 
-// Fetch para servir recursos cacheados o en línea
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
